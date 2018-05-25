6f1d05943bb96d44ea0bc30aedf35b2a2a620fb3
Mark Lawley <mark.lawley@gmail.com>
2018-05-25 17:56:43 -0400

    Hibernate entity fix

--- thegame/src/main/java/gameCore/CharacterSheet.java
+++ thegame/src/main/java/gameCore/CharacterSheet.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Character {
+public class CharacterSheet {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer characterId;
