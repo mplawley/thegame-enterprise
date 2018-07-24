7c99d733edbff34ff0bd79e48da831a38e9db154
Mark Lawley <mark.lawley@gmail.com>
2018-07-23 20:49:20 -0400

    Inventory buildout to completion

--- backend/src/test/java/gameCore/characterSheet/CharacterSheetControllerTest.java
+++ backend/src/test/java/gameCore/characterSheet/CharacterSheetControllerTest.java
@@ -1,8 +1,10 @@
 package gameCore.characterSheet;
 
+import gameCore.characterSheetVitals.CharacterSheetVitals;
 import mockit.Injectable;
 import mockit.Tested;
 import mockit.Verifications;
+import mockit.VerificationsInOrder;
 import org.junit.Test;
 
 public class CharacterSheetControllerTest {
@@ -36,4 +38,51 @@ public class CharacterSheetControllerTest {
             times = 1;
         }};
     }
+
+    @Test
+    public void saveVitalsWithValidInput() {
+        Integer currentLife = 100;
+        Integer maxLife = 100;
+        Integer currentEndurance = 10;
+        Integer maxEndurance = 10;
+        CharacterSheetVitals characterSheetVitals = new CharacterSheetVitals(TEST_CHARACTER_ID, currentLife, maxLife, currentEndurance, maxEndurance);
+
+        characterSheetController.saveVitals(TEST_CHARACTER_ID, currentLife, maxLife, currentEndurance, maxEndurance);
+
+        new Verifications() {{
+            characterSheetService.saveVitals((CharacterSheetVitals)any);
+            times = 1;
+        }};
+    }
+
+    @Test
+    public void getVitalsWithValidCharacterId() {
+        characterSheetController.getVitals(TEST_CHARACTER_ID);
+
+        new Verifications() {{
+            characterSheetService.getVitals(TEST_CHARACTER_ID);
+            times = 1;
+        }};
+    }
+
+    @Test
+    public void getStatsWithValidCharacterId() {
+        characterSheetController.getStats(TEST_CHARACTER_ID);
+
+        new Verifications() {{
+            characterSheetService.getStats(TEST_CHARACTER_ID);
+            times = 1;
+        }};
+    }
+
+    @Test
+    public void saveEntryTest() {
+        characterSheetController.saveEntryTest();
+
+        new Verifications() {{
+            characterSheetService.saveEntryTest();
+            times = 1;
+        }};
+    }
+
 }
--- backend/src/test/java/gameCore/characterSheet/CharacterSheetServiceTest.java
+++ backend/src/test/java/gameCore/characterSheet/CharacterSheetServiceTest.java
@@ -1,44 +1,26 @@
-//package gameCore.characterSheet;
-//
-//import mockit.Injectable;
-//import mockit.Tested;
-//import mockit.Verifications;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
-//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//@RunWith(SpringRunner.class)
-//@DataJpaTest
-//@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
-//public class CharacterSheetServiceTest {
-//    @Tested
-//    CharacterSheetServiceImpl characterSheetServiceImpl;
-//
-//    @Autowired
-//    CharacterSheetRepository characterSheetRepository;
-//
-//    private final Long TEST_CHARACTER_ID = 123L;
-//
-//    @Test
-//    public void getCharacterSheetByIdCallsFindByCharacterIdOnCharacterSheetRepository() {
-//        characterSheetServiceImpl.getCharacterSheet(TEST_CHARACTER_ID);
-//
-//        new Verifications() {{
-//            characterSheetRepository.findByCharacterId(TEST_CHARACTER_ID);
-//            times = 1;
-//        }};
-//    }
-//
-//    @Test
-//    public void myTest() throws Exception {
-//        CharacterSheet characterSheet = new CharacterSheet();
-//        characterSheet.setCharacterId(1L);
-//        characterSheetRepository.save(characterSheet);
-//    }
-//}
-//
-//
+package gameCore.characterSheet;
+
+import mockit.Mocked;
+import mockit.Tested;
+import mockit.Verifications;
+import org.junit.Test;
+
+public class CharacterSheetServiceTest {
+    @Tested
+    CharacterSheetServiceImpl characterSheetServiceImpl;
+
+    @Mocked
+    CharacterSheetRepository characterSheetRepository;
+
+    private final Long TEST_CHARACTER_ID = 123L;
+
+    @Test
+    public void getCharacterSheetByIdCallsFindByCharacterIdOnCharacterSheetRepository() {
+        characterSheetServiceImpl.getCharacterSheet(TEST_CHARACTER_ID);
+
+        new Verifications() {{
+            characterSheetRepository.findByCharacterId(TEST_CHARACTER_ID);
+            times = 1;
+        }};
+    }
+}
