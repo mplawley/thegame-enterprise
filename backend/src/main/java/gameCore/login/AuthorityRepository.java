2d2d646728536a2722bd385348849820fb6e5aa5
Mark Lawley <mark.lawley@gmail.com>
2018-09-12 22:18:10 -0400

    Basic setup

---
+++ backend/src/main/java/gameCore/login/AuthorityRepository.java
@@ -0,0 +1,7 @@
+package gameCore.login;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorityRepository extends CrudRepository<Authority, String> {
+
+}
---
+++ backend/src/main/java/gameCore/login/UserRepository.java
@@ -0,0 +1,23 @@
+package gameCore.login;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, String> {
+    List<User> findAllByActivatedIsFalseAndCreatedDateBefore(Instant dateTime);
+
+    Optional<User> findOneByEmailIgnoreCase(String email);
+
+    Optional<User> findOneByLogin(String login);
+
+    Page<User> findAllByLoginNot(Pageable pageable, String login);
+
+}
+
